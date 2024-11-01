d4130f6949804b8bc6ba65738634a104fce8680a
devhoss <velasco2288@gmail.com>
2024-11-01 11:35:28 -0500

    first commit

---
+++ src/main/java/com/devhoss/traverse/TraverseApiApplication.java
@@ -0,0 +1,13 @@
+package com.devhoss.traverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraverseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devhoss/traverse/TraverseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhoss.traverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraverseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
